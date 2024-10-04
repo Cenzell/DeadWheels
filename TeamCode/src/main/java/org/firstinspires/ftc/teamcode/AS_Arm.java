7884d310a1a402cd63a9c3c7ee314c4c74842e9b
cenzell <ethanhartshorn488@outlook.com>
2024-10-04 11:05:21 -0500

    Working Arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AS_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AS_Arm.java
@@ -7,19 +7,23 @@ import com.arcrobotics.ftclib.controller.PIDFController;
 import com.arcrobotics.ftclib.util.InterpLUT;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
 @TeleOp(name="AS_Arm")
 public class AS_Arm extends OpMode {
 
+    //HardwareMap hardwareMap;
+
     InterpLUT IntP = new InterpLUT(), IntI = new InterpLUT(), IntD = new InterpLUT(), IntF = new InterpLUT();
 
-    DcMotorEx armMotor;
-    DcMotorEx extenderLeft, extenderRight; //TODO Check 560 ticks per rev
+    DcMotor armMotorLeft;
+    DcMotor armMotorRight;
+    //DcMotorEx extenderLeft, extenderRight; //TODO Check 560 ticks per rev
     //TODO Limit C=42/cos(x)
-
 
     PIDFController Pid;
 
@@ -33,19 +37,21 @@ public class AS_Arm extends OpMode {
 
     @Override
     public void init() {
+
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        armMotor = hardwareMap.get(DcMotorEx.class, "BigArm");
+        armMotorRight = hardwareMap.get(DcMotor.class, "BigArmRight");
+        armMotorLeft = hardwareMap.get(DcMotor.class, "BigArmLeft");
 
-        extenderLeft = hardwareMap.get(DcMotorEx.class, "ExtenderLeft");
-        extenderRight = hardwareMap.get(DcMotorEx.class, "ExtenderRight");
+        //extenderLeft = hardwareMap.get(DcMotorEx.class, "ExtenderLeft");
+        //extenderRight = hardwareMap.get(DcMotorEx.class, "ExtenderRight");
 
         //TODO - Not sure which one needs to be reversed.
         //extenderLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         //extenderRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         pid1 = new double[]{0.0001, 0, 0, 0};
-        pid2 = new double[]{1, 1, 1, 1};
+        pid2 = new double[]{0.0002, 0, 0, 0};
 
 //        IntP.add(-55, pid1[0]);
 //        IntP.add(365, pid1[0]);
@@ -74,8 +80,10 @@ public class AS_Arm extends OpMode {
 
     @Override
     public void loop() {
-        double armPos = armMotor.getCurrentPosition();
-        double armDeg = ((armMotor.getCurrentPosition() * TICKS_PER_DEGREE) % 360);
+        FtcDashboard.getInstance().updateConfig();
+
+        double armPos = -armMotorLeft.getCurrentPosition();
+        double armDeg = ((-armMotorLeft.getCurrentPosition() * TICKS_PER_DEGREE) % 360);
 
 //        Pid.setPIDF(
 //                IntP.get(armDeg),
@@ -87,26 +95,34 @@ public class AS_Arm extends OpMode {
 
         //TODO Add extension and get a extension PID - Kinda my whole idea behind using interpolation.
 
-        if(gamepad2.a){ //Arm with Dashboard PID
+        if(gamepad1.a){ //Arm with Dashboard PID
             double output = Pid.calculate(armDeg, target); //Will be pid1 setpoint
-            armMotor.setPower(output);
-        }else if (gamepad2.y){
+            arm(output);
+        }else if (gamepad1.y){
             double output = Pid.calculate(armPos,0); //pid[x] setpoint //TODO Use to get more pid value ranges
-            armMotor.setPower(output);
-        } else if (gamepad2.b){
+            arm(output);
+        } else if (gamepad1.b){
             //armMotor.setPower(IntF.get(armMotor.getCurrentPosition()));
-            armMotor.setPower(Math.cos(Math.toRadians(target/TICKS_PER_DEGREE)) * ff);
-        } else if (gamepad2.x){
+            arm(Math.cos(Math.toRadians(target/TICKS_PER_DEGREE)) * ff);
+        } else if (gamepad1.x){
             IntP.createLUT(); IntI. createLUT(); IntD.createLUT(); IntF.createLUT();
         } else { //Not needed but to ensure that if there is no input the power is zero.
-            armMotor.setPower(0);
+            arm(0);
         }
+
+//        if(gamepad2.dpad_up){
+//            extender(0.2);
+//        } else if (gamepad2.dpad_down) {
+//            extender(-0.2);
+//        } else {
+//            extender(0);
+//        }
 
         FtcDashboard.getInstance().updateConfig();
 
-        telemetry.addData("Encoder Pos:", armMotor.getCurrentPosition());
+        telemetry.addData("Encoder Pos:", armPos);
         telemetry.addData("Setpoint:", target);
-        telemetry.addData("Deg", (armMotor.getCurrentPosition() * TICKS_PER_DEGREE) % 360);
+        telemetry.addData("Deg", armDeg);
         telemetry.addData("Output:", Pid.calculate(armPos,target));
 
         telemetry.addData("IntP:", IntP.get(armDeg));
@@ -119,12 +135,16 @@ public class AS_Arm extends OpMode {
         telemetry.addData("D:", Pid.getD());
         telemetry.addData("F:", Pid.getF());
 
-
-        extender(0);
+        //extender(0);
     }
 
-    public void extender(double power){
-        extenderLeft.setPower(power);
-        extenderRight.setPower(power);
+//    public void extender(double power){
+//        extenderLeft.setPower(power);
+//        extenderRight.setPower(-power);
+//    }
+
+    public void arm(double power){
+        armMotorLeft.setPower(-power);
+        armMotorRight.setPower(power);
     }
 }
